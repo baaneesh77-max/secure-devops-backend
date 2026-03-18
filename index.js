2a49fc036585ac57fd723b63c7f1f4f2b0b12638
Aneesh Balaji <baaneesh77@gmail.com>
2026-03-18 12:21:45 +0000

    Initial backend app

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+app.get('/search', (req, res) => {
+  const q = req.query.q || '';
+  res.send(`You searched for: ${q}`);
+});
+
+app.post('/dangerous-eval', (req, res) => {
+  const code = req.body.code;
+  const result = eval(code);
+  res.json({ result });
+});
+
+app.get('/profile', (req, res) => {
+  res.send(`
+    <html>
+      <head><title>Profile</title></head>
+      <body>
+        <h1>User Profile</h1>
+        <form>
+          <input name="username" />
+          <button type="submit">Save</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
